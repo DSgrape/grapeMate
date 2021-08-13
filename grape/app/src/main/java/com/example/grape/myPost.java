6b9f50dfe99515ea1e91bb41b61a465a860e864f
tndus03 <73951358+tndus03@users.noreply.github.com>
2021-08-13 12:35:45 +0900

    내가 쓴 글
    내가 쓴 글 java 파일

---
+++ grape/app/src/main/java/com/example/grape/myPost.java
@@ -0,0 +1,23 @@
+package com.example.grape;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.fragment.app.Fragment;
+
+public class myPost extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View v = inflater.inflate(R.layout.my_post, container, false);
+
+        return v;
+    }
+}
